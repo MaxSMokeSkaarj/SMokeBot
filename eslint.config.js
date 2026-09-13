d29b37c113c77e99021cad8a4d8d863dec20f356
Valeriy <maxsmokeskaaj@gmail.com>
2026-09-13 18:31:14 +1000

    fix: configure eslint for Node runtime

--- eslint.config.js
+++ eslint.config.js
@@ -8,15 +8,15 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+    },
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
       indent: ['error', 2],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^ctx$' }],
     },
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: { globals: globals.browser },
   },
   {
     files: ['**/*.json'],
